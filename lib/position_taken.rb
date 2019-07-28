2628e1c6272b1e82bc3d51c45940d24331a490f2
erin johnson <erin.nicole.johnson@gmail.com>
2019-07-28 11:37:33 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,9 +1,10 @@
 # code your #position_taken? method here!
 
 def position_taken?(board, index)
-  if board[index] != "X" || "O"
+  if board[index] != "X" || board[index] != "O"
     false
   else
     true
   end
+  puts board
 end
